08a6fc308d2136e3d6c49c5df1dd246839aed771
filda1 <38835920+filda1@users.noreply.github.com>
2020-10-09 18:00:00 +0100

    error overflow

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -14,31 +14,37 @@ class WelcomePage extends StatefulWidget {
 
 class _WelcomePageState extends State<WelcomePage> {
   Widget _submitButton() {
-    return InkWell(
-      onTap: () {
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => LoginPage()));
-      },
-      child: Container(
-        width: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.symmetric(vertical: 13),
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(5)),
-            boxShadow: <BoxShadow>[
-              BoxShadow(
-                  color: Color(0xffdf8e33).withAlpha(100),
-                  offset: Offset(2, 4),
-                  blurRadius: 8,
-                  spreadRadius: 2)
-            ],
-            color: Colors.white),
-        child: Text(
-          'Login',
-          style: TextStyle(fontSize: 20, color: Color(0xfff0091ea)),
+    return
+     Center(
+       child: InkWell(
+        onTap: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => LoginPage()));
+        },
+        child: 
+        Center(
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.symmetric(vertical: 13),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(5)),
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                      color: Color(0xffdf8e33).withAlpha(100),
+                      offset: Offset(2, 4),
+                      blurRadius: 8,
+                      spreadRadius: 2)
+                ],
+                color: Colors.white),
+            child: Text(
+              'Login',
+              style: TextStyle(fontSize: 20, color: Color(0xfff0091ea)),
+            ),
+          ),
         ),
-      ),
-    );
+    ),
+     );
   }
 
   Widget _signUpButton() {
